20981aa3046ec9bb78d06a8e210b5e3063aa1902
Eyüp Kaan Özteriş <68303307+EyupKaan@users.noreply.github.com>
2024-03-13 17:49:21 +0300

    Hello World
    Trying to work on same project with different ide's.

---
+++ java-001-hello-world/src/main/java/org/eyupkaan/AppMain.java
@@ -0,0 +1,9 @@
+package org.eyupkaan;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This line written by Eclipse.");
+        System.out.println("This line written by Intellij IDEA.");
+    }
+}
